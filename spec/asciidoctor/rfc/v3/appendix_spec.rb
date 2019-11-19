ea5c39872ec1b74c5931925ca11e5ac8a0245bee
Nick Nicholas <opoudjis@gmail.com>
2019-11-19 19:49:38 +1100

    Revert "Extract RFC bib fetching / caching / citation functionality into relaton-ietf #3"

--- spec/asciidoctor/rfc/v3/appendix_spec.rb
+++ spec/asciidoctor/rfc/v3/appendix_spec.rb
@@ -37,94 +37,92 @@ RSpec.describe Asciidoctor::Rfc::V3::Converter do
     OUTPUT
   end
   it "renders appendix when section follows references" do
-    VCR.use_cassette "rfc3_appendix" do
-      expect(Asciidoctor.convert(<<~"INPUT", backend: :rfc3, header_footer: true)).to be_equivalent_to <<~"OUTPUT"
-        = Document title
-        :docName:
-        Author
+    expect(Asciidoctor.convert(<<~"INPUT", backend: :rfc3, header_footer: true)).to be_equivalent_to <<~"OUTPUT"
+      = Document title
+      :docName:
+      Author
 
-        == Section 1
-        text
+      == Section 1
+      text
 
-        [bibliography]
-        == Biblio
-        ++++
-        <reference anchor='ISO.IEC.10118-3' target='https://www.iso.org/standard/67116.html'>
-          <front>
-            <title>ISO/IEC FDIS 10118-3 -- Information technology -- Security techniques -- Hash-functions -- Part 3: Dedicated hash-functions</title>
-            <author>
-              <organization>International Organization for Standardization</organization>
-              <address>
-                <postal>
-                  <street>BIBC II</street>
-                  <street>Chemin de Blandonnet 8</street>
-                  <street>CP 401</street>
-                  <city>Vernier</city>
-                  <region>Geneva</region>
-                  <code>1214</code>
-                  <country>Switzerland</country>
-                </postal>
-                <phone>+41 22 749 01 11</phone>
-                <email>central@iso.org</email>
-                <uri>https://www.iso.org/</uri>
-              </address>
-            </author>
-            <date day='15' month='September' year='2017'/>
-          </front>
-        </reference>
-        ++++
-
-        == Appendix
-        text
-      INPUT
-        #{XML_HDR}
-
-        <rfc prepTime="2000-01-01T05:00:00Z"
-                  version="3" submissionType="IETF">
+      [bibliography]
+      == Biblio
+      ++++
+      <reference anchor='ISO.IEC.10118-3' target='https://www.iso.org/standard/67116.html'>
         <front>
-        <title>Document title</title>
-        <author fullname="Author">
-        </author>
-        <date day="1" month="January" year="2000"/>
-        </front><middle>
-        <section anchor="_section_1" numbered="false">
-        <name>Section 1</name>
-        <t>text</t>
-        </section>
-        </middle><back>
-        <references anchor="_biblio">
-        <name>Biblio</name>
-        <reference anchor='ISO.IEC.10118-3' target='https://www.iso.org/standard/67116.html'>
-          <front>
-            <title>ISO/IEC FDIS 10118-3 -- Information technology -- Security techniques -- Hash-functions -- Part 3: Dedicated hash-functions</title>
-            <author>
-              <organization>International Organization for Standardization</organization>
-              <address>
-                <postal>
-                  <street>BIBC II</street>
-                  <street>Chemin de Blandonnet 8</street>
-                  <street>CP 401</street>
-                  <city>Vernier</city>
-                  <region>Geneva</region>
-                  <code>1214</code>
-                  <country>Switzerland</country>
-                </postal>
-                <phone>+41 22 749 01 11</phone>
-                <email>central@iso.org</email>
-                <uri>https://www.iso.org/</uri>
-              </address>
-            </author>
-            <date day='15' month='September' year='2017'/>
-          </front>
-        </reference>
-        </references>
-        <section anchor="_appendix" numbered="false">
-        <name>Appendix</name>
-        <t>text</t>
-        </section>
-        </back>
-        </rfc>
-      OUTPUT
-    end
+          <title>ISO/IEC FDIS 10118-3 -- Information technology -- Security techniques -- Hash-functions -- Part 3: Dedicated hash-functions</title>
+          <author>
+            <organization>International Organization for Standardization</organization>
+            <address>
+              <postal>
+                <street>BIBC II</street>
+                <street>Chemin de Blandonnet 8</street>
+                <street>CP 401</street>
+                <city>Vernier</city>
+                <region>Geneva</region>
+                <code>1214</code>
+                <country>Switzerland</country>
+              </postal>
+              <phone>+41 22 749 01 11</phone>
+              <email>central@iso.org</email>
+              <uri>https://www.iso.org/</uri>
+            </address>
+          </author>
+          <date day='15' month='September' year='2017'/>
+        </front>
+      </reference>
+      ++++
+
+      == Appendix
+      text
+    INPUT
+      #{XML_HDR}
+
+      <rfc prepTime="2000-01-01T05:00:00Z"
+                version="3" submissionType="IETF">
+      <front>
+      <title>Document title</title>
+      <author fullname="Author">
+      </author>
+      <date day="1" month="January" year="2000"/>
+      </front><middle>
+      <section anchor="_section_1" numbered="false">
+      <name>Section 1</name>
+      <t>text</t>
+      </section>
+      </middle><back>
+      <references anchor="_biblio">
+      <name>Biblio</name>
+      <reference anchor='ISO.IEC.10118-3' target='https://www.iso.org/standard/67116.html'>
+        <front>
+          <title>ISO/IEC FDIS 10118-3 -- Information technology -- Security techniques -- Hash-functions -- Part 3: Dedicated hash-functions</title>
+          <author>
+            <organization>International Organization for Standardization</organization>
+            <address>
+              <postal>
+                <street>BIBC II</street>
+                <street>Chemin de Blandonnet 8</street>
+                <street>CP 401</street>
+                <city>Vernier</city>
+                <region>Geneva</region>
+                <code>1214</code>
+                <country>Switzerland</country>
+              </postal>
+              <phone>+41 22 749 01 11</phone>
+              <email>central@iso.org</email>
+              <uri>https://www.iso.org/</uri>
+            </address>
+          </author>
+          <date day='15' month='September' year='2017'/>
+        </front>
+      </reference>
+      </references>
+      <section anchor="_appendix" numbered="false">
+      <name>Appendix</name>
+      <t>text</t>
+      </section>
+      </back>
+      </rfc>
+    OUTPUT
   end
 end
