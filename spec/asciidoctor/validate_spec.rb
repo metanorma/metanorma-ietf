f5dc7b053859b67a9dc34b34713551a1bbea2cfb
Nick Nicholas <opoudjis@gmail.com>
2020-10-11 14:45:10 +1100

    https://github.com/metanorma/metanorma-standoc/issues/225

--- spec/asciidoctor/validate_spec.rb
+++ spec/asciidoctor/validate_spec.rb
@@ -58,4 +58,26 @@ RSpec.describe Asciidoctor::Ietf do
   end
   end
 
+  context "when xref_error.adoc compilation" do
+    around do |example|
+      FileUtils.rm_f "spec/assets/xref_error.err"
+      example.run
+      Dir["spec/assets/xref_error*"].each do |file|
+        next if file.match?(/adoc$/)
+
+        FileUtils.rm_f(file)
+      end
+    end
+
+    it "generates error file" do
+      expect do
+        Metanorma::Compile
+          .new
+          .compile("spec/assets/xref_error.adoc", type: "ietf")
+      end.to(change { File.exist?("spec/assets/xref_error.err") }
+              .from(false).to(true))
+    end
+  end
+
+
 end
