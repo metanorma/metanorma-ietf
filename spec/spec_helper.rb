d0f153461a2f39b201f991edd2ab6b3d0d324aa6
Nick Nicholas <opoudjis@gmail.com>
2022-11-15 02:50:02 +1100

    remove rexml from rspec: https://github.com/metanorma/isodoc/issues/260

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,24 +52,20 @@ end
 
 OPTIONS = [backend: :ietf, header_footer: true].freeze
 
-def htmlencode(x)
-  HTMLEntities.new.encode(x, :hexadecimal).gsub(/&#x3e;/, ">").gsub(/&#xa;/, "\n")
+def htmlencode(xml)
+  HTMLEntities.new.encode(xml, :hexadecimal).gsub(/&#x3e;/, ">").gsub(/&#xa;/, "\n")
     .gsub(/&#x22;/, '"').gsub(/&#x3c;/, "<").gsub(/&#x26;/, "&").gsub(/&#x27;/, "'")
     .gsub(/\\u(....)/) do |_s|
     "&#x#{$1.downcase};"
   end
 end
 
-def strip_guid(x)
-  x.gsub(%r{ id="_[^"]+"}, ' id="_"').gsub(%r{ target="_[^"]+"}, ' target="_"')
+def strip_guid(xml)
+  xml.gsub(%r{ id="_[^"]+"}, ' id="_"').gsub(%r{ target="_[^"]+"}, ' target="_"')
 end
 
 def xmlpp(xml)
-  s = ""
-  f = REXML::Formatters::Pretty.new(2)
-  f.compact = true
-  f.write(REXML::Document.new(xml), s)
-  s
+  Nokogiri::XML(xml).to_xml(indent: 2, encoding: "UTF-8")
 end
 
 def dtd_absolute_path
